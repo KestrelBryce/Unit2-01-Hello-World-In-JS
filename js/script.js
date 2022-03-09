f0141b85a83848a59874b19c53ddf492a100268f
KestrelBryce <99274358+KestrelBryce@users.noreply.github.com>
2022-03-09 16:06:28 +0000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+function myButtonClicked () {
+  // this function displays "Message"
+  
+  document.getElementById('Message').innerHTML = 'I am safe for now...'
+}
+function myButtonClicked2 () {
+  // this function displays "Second Message"
+  
+  document.getElementById('Message-2').innerHTML = 'I finally found some other survivors.'
+}
+function myButtonClicked3 () {
+  // this function displays "Final Message"
+  
+  document.getElementById('Message-3').innerHTML = 'The walls are still moving, we are not safe here.'
+}
+function myButtonClicked4 () {
+  // this function displays "Final Message"
+  
+  document.getElementById('Message-4').innerHTML = 'We need to get out of this school, but how?'
+}
