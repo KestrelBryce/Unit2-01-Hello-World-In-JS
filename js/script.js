d0968e1cfeb8e4ce770993e08466d379304b49d0
Kestrel.Bryce <99274358+KestrelBryce@users.noreply.github.com>
2022-03-09 23:42:30 +0000

    Finishing touches, added favicon, edited text, etc

--- js/script.js
+++ js/script.js
@@ -2,20 +2,20 @@
 function myButtonClicked () {
   // this function displays "Message"
   
-  document.getElementById('Message').innerHTML = 'I am safe for now...'
+  document.getElementById('Message').innerHTML = 'I am trapped here, but at least I am safe for now...'
 }
 function myButtonClicked2 () {
   // this function displays "Second Message"
   
-  document.getElementById('Message-2').innerHTML = 'I finally found some other survivors.'
+  document.getElementById('Message-2').innerHTML = 'I finally found some other survivors. They managed to lose those things in the halls. We are battered and tired, but at least we are alive.'
 }
 function myButtonClicked3 () {
   // this function displays "Final Message"
   
-  document.getElementById('Message-3').innerHTML = 'The walls are still moving, we are not safe here.'
+  document.getElementById('Message-3').innerHTML = 'The walls are still moving, though, we are not safe here. Those things could burst out any minute.'
 }
 function myButtonClicked4 () {
   // this function displays "Final Message"
   
-  document.getElementById('Message-4').innerHTML = 'We need to get out of this school, but how?'
+  document.getElementById('Message-4').innerHTML = 'We need to get out of this school and get some help, the police, the army, something, anything! But how?'
 }
